2e9dacaa79d6a98daa8b1bfa90f2ff2ec6eb126a
iofabela <israel.fabelaperez@hotmail.com>
2026-06-21 09:52:09 -0600

    Code for course of llm-zoomcamp

---
+++ 05-monitoring/assistant.py
@@ -0,0 +1,5 @@
+'''
+PENDING MODULE FOR MODULE 5 [use the next url to check trace and monitoring] : 
+[video] : https://www.youtube.com/watch?v=ImY5-Q97sRw&list=PL3MmuxUbc_hJAmLLf2x1LSKRKbZwKXoHd
+[repo] : https://github.com/DataTalksClub/llm-zoomcamp/blob/main/05-monitoring/code/assistant.py
+'''
